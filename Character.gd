490666e16ada40ebf9cd990e22af9897f98a62fe
= <josh.carlson4061@gmail.com>
2022-09-30 15:35:03 -0700

    Correct shadow shader

--- Character.gd
+++ Character.gd
@@ -5,6 +5,7 @@ export var move_speed := 5000.0
 var shear := 0.5
 
 onready var poly = get_node("Polygon2D")
+onready var spr = get_node("Sprite")
 
 func _physics_process(delta):
 	var input := Vector2(Input.get_axis("move_left", "move_right"), Input.get_axis("move_up", "move_down"))
@@ -12,8 +13,3 @@ func _physics_process(delta):
 		input = input.normalized()
 	
 	move_and_slide(input * move_speed * delta)
-
-#func _process(delta):
-#	var t = poly.transform
-#	t.y = Vector2(shear, 1.0)
-#	poly.transform = t
