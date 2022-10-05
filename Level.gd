b0f0ffa355fd22361a06764122e1f78a29e5203c
Josh Carlson <josh.carlson4061@gmail.com>
2022-10-05 12:25:41 -0700

    Get pixelate kinda working

--- Level.gd
+++ Level.gd
@@ -5,5 +5,7 @@ onready var sv : Viewport = get_node("ShadowViewport")
 
 func _ready():
 	for s in get_tree().get_nodes_in_group("shadow"):
+		var loc = s.global_position
 		s.get_parent().remove_child(s)
 		sv.add_child(s)
+		s.global_position = loc
